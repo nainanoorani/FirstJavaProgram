dbf302bf90d79cc40a609b4c1a2055df80990e76
Naina Noorani <nainanoorani@gmail.com>
2022-09-19 21:09:08 -0500

    initial commit

---
+++ src/FirstJavaClass.java
@@ -0,0 +1,14 @@
+public class FirstJavaClass {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("Hello Java");
+        System.out.println("Howdy Devmountain");
+        //Inline comment
+        /**
+         * stored in project documentation
+         */
+        /*shift command backslash for multi
+        * line
+        * comment*/
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,16 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Variables {
+    //type main then ctrl +space to get the default code
+    public static void main(String[] args) {
+        int firstVariable;
+        firstVariable = 10;
+        int secondVariable =20;
+        int total = firstVariable+secondVariable;
+        System.out.println("First variable: " + firstVariable);
+        System.out.println("Total: " + total);
+    }
+
+
+
+}
